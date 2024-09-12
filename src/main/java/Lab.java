a5633ce7ebf6626bd079d2a8afd9a8a893341e6e
Daniel Olen <danolen89@gmail.com>
2024-09-12 14:50:28 +0000

    Coding Lab: Lists Completed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,5 +1,5 @@
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Lists are ordered collections. This means that we could access elements at a certain position of a list,
@@ -26,7 +26,9 @@ public class Lab {
      * @return a List<Integer> object.
      */
     public List<Integer> createList(){
-        return null;
+        List<Integer> listOfInts = new ArrayList<>();
+
+        return listOfInts;
     }
 
     /**
@@ -36,7 +38,7 @@ public class Lab {
      * @return the size of List (number of items it holds.)
      */
     public int getSize(List<Integer> list){
-        return 0;
+        return list.size();
     }
 
     /**
@@ -48,6 +50,7 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addToList(List<Integer> list, int value){
+        list.add(value);
     }
 
     /**
@@ -65,7 +68,7 @@ public class Lab {
      * @return the int value at the location in 'list' represented by 'index'.
      */
     public int get(List<Integer> list, int index){
-        return 0;
+        return list.get(index);    
     }
 
     /**
@@ -77,9 +80,14 @@ public class Lab {
      * @param position represents the index of the element we would like to remove.
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
-    public void removeFromList(List<Integer> list, int position){
-
+    public void removeFromList(List<Integer> list, int position) {
+    if (position >= 0 && position < list.size()) {
+        list.remove(position);
+    } else {
+        System.out.println("Invalid position");
     }
+}
+
 
     /**
      * Update an index of a list.
@@ -90,6 +98,11 @@ public class Lab {
      * @param value the new value which we would like to assign to the item at position in list
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
-    public void updateAtPosition(List<Integer> list, int position, int value){
+    public void updateAtPosition(List<Integer> list, int position, int value) {
+    if (position >= 0 && position < list.size()) {
+        list.set(position, value);
+    } else {
+        System.out.println("Invalid position");
     }
+}
 }
